eecb2cd42226b78f0c72ba308a4a72ecfcea78f4
Trần Hoàng Hiệp <hiepthph25029@fpt.edu.vn>
2024-05-18 23:43:38 +0700

    Layout config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
 
 function App() {
     return (
@@ -9,7 +10,13 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        const Layout = route.layout || DefaultLayout;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
                         return (
                             <Route
                                 key={index}
